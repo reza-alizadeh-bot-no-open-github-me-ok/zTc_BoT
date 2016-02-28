ca0abe8f857dc25be30cfda85b687daa87baa18d
reza-alizadeh-bot-no-open-github-me-ok <c9.io.acc@gmail.com>
2016-02-28 16:19:46 +0330

    Up

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -6,8 +6,8 @@ do
  
  
    local text = matches[1]
-   local chat = "chat#id"..158990680 
-   --like : local chat = "chat#id"..12345678
+   local chat = "chat#id"..132557188
+   --like : local chat = "chat#id"..132557188
    
   local sends = send_msg(chat, fuse, ok_cb, false)
   return 'بازخورد شما ارسال شد'
