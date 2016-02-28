765c6a6aca9566fb6ff226fcfadd3a6b664df317
reza-alizadeh-bot-no-open-github-me-ok <c9.io.acc@gmail.com>
2016-02-28 16:08:01 +0330

    up

--- plugins/info.lua
+++ plugins/info.lua
@@ -123,11 +123,11 @@ local function action_by_reply(extra, success, result)-- (reply) /info  function
   local um_hash = 'msgs:'..result.from.id..':'..result.to.id
   user_info_msgs = tonumber(redis:get(um_hash) or 0)
   text = text..'تعداد پیام های فرستاده شده : '..user_info_msgs..'\n\n'
-  text = text..'nod32 edited version'
+  text = text..'*-*'
   send_msg(extra.receiver, text, ok_cb, true)
 end
 
-local function action_by_reply2(extra, success, result)
+local function action_by_reply2(extra success, result)
 local value = extra.value
 setrank(result, result.from.id, value)
 end
